6eb7113fcb36a333c73e6b388be05ef0c0e5eee0
amochuko <7204868+amochuko@users.noreply.github.com>
2025-12-18 13:44:28 +0100

    initial commit

---
+++ apps/swarm-registry/hardhat.config.ts
@@ -0,0 +1,47 @@
+import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import { configVariable, defineConfig } from "hardhat/config";
+
+export default defineConfig({
+  plugins: [hardhatToolboxMochaEthersPlugin],
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.28",
+      },
+      production: {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1", // Ethereum mainnet and its testnets
+    },
+    amoy: {
+      type: "http",
+      chainType: "generic",
+      url: configVariable("AMOY_RPC_URL"),
+      accounts: [configVariable("AMOY_PRIVATE_KEY")],
+      ignition: {},
+    },
+    polygon: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("POLYGON_RPC_URL"),
+      accounts: [configVariable("POLYGON_PRIVATE_KEY")],
+      ignition: {},
+    },
+  },
+  verify: {
+    etherscan: {
+      apiKey: configVariable("ETHERSCAN_API_KEY"),
+    },
+  },
+});
