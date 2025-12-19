d75f1d00e274a4c8a8310dd27246b8d9a95a1928
amochuko <7204868+amochuko@users.noreply.github.com>
2025-12-19 10:19:04 +0100

    add comment

--- apps/swarm-registry/contracts/SwarmRegistry.sol
+++ apps/swarm-registry/contracts/SwarmRegistry.sol
@@ -4,13 +4,24 @@ pragma solidity ^0.8.18;
 import {IRegistry} from "./interface/IRegistry.sol";
 
 /// @title SwarmRegistry
-/// @notice Minimal registry to publish Swarm BZZ manifest hashes and metadata references on-chain.
+/// @notice Minimal registry to publish Swarm BZZ manifest hashes and metadata references on-chain. it
+/// supports both direct publishing and signature-based relay publishing.
 contract SwarmRegistry is IRegistry {
+    /*//////////////////
+            EVENTS
+    //////////////////*/
     event ManifestPublished(
         address indexed publisher, bytes32 indexed bzzHashed, string metadataUri, uint256 timestamp
     );
 
+    /*///////////////////////////
+            STORAGE
+    //////////////////////////*/
+
+    // @notice Swarm manifest hash => original publisher
     mapping(bytes32 => address) public publisherOf;
+
+    /// Swarm manifest hash => metadata URI
     mapping(bytes32 => string) public metadataOf;
 
     modifier isRegistered(bytes32 bzzHash) {
@@ -23,6 +34,13 @@ contract SwarmRegistry is IRegistry {
         _;
     }
 
+    /*////////////////////////////////
+                    DIRECT PUBLIC
+    ///////////////////////////////*/
+
+    /// @notice Publish a Swarm manifest directly (caller pays gas)
+    /// @param bzzHash the reference hash from uploaded data via Swarm
+    /// @param metadataUri metadataURI as refrenced by bzzHash
     function publishManifest(bytes32 bzzHash, string calldata metadataUri) external isRegistered(bzzHash) {
         require(bzzHash != bytes32(0), "Invalide hash");
 
@@ -32,14 +50,20 @@ contract SwarmRegistry is IRegistry {
         emit ManifestPublished(msg.sender, bzzHash, metadataUri, block.timestamp);
     }
 
+    /// @notice Get metadata
+    /// @param bzzHash The reference hash
     function getMetadata(bytes32 bzzHash) external view returns (string memory) {
         return metadataOf[bzzHash];
     }
 
+    /// @notice Get publisher of a manifest
+    /// @param bzzHash The reference hash
     function getPublisher(bytes32 bzzHash) external view returns (address) {
         return publisherOf[bzzHash];
     }
 
+    /// @notice Update metadata
+    /// @param bzzHash The reference hash
     function updateMetadata(bytes32 bzzHash, string calldata metadataUri) external isPublisher(bzzHash) {
         metadataOf[bzzHash] = metadataUri;
     }
