664c93c7c7b6655494b41a87cfae6ebb912dd4dd
amochuko <7204868+amochuko@users.noreply.github.com>
2025-12-19 06:42:49 +0100

    add updateMetadata

--- apps/swarm-registry/contracts/interface/IRegistry.sol
+++ apps/swarm-registry/contracts/interface/IRegistry.sol
@@ -5,4 +5,5 @@ interface IRegistry {
     function publishManifest(bytes32 bzzHash, string calldata metadataUri) external;
     function getMetadata(bytes32 bzzHash) external view returns (string memory);
     function getPublisher(bytes32 bzzHash) external view returns (address);
+    function updateMetadata(bytes32 bzzHash, string calldata metadataUri) external;
 }
