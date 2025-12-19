a0beea64221790c29ff285257992bb5a66c17afd
amochuko <7204868+amochuko@users.noreply.github.com>
2025-12-19 05:27:56 +0100

    fix typo

--- apps/swarm-registry/contracts/SwarmRegistry.sol
+++ apps/swarm-registry/contracts/SwarmRegistry.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.18;
 
+import {IRegistry} from "./interface/IRegistry.sol";
+
 /// @title SwarmRegistry
 /// @notice Minimal registry to publish Swarm BZZ manifest hashes and metadata references on-chain.
-contract SwarmRegistry {
+contract SwarmRegistry is IRegistry {
     event ManifestPublished(
         address indexed publisher, bytes32 indexed bzzHashed, string metadataUri, uint256 timestamp
     );
@@ -11,5 +13,12 @@ contract SwarmRegistry {
     mapping(bytes32 => address) public publisherOf;
     mapping(bytes32 => string) public metadataOf;
 
+    function publishMenifest(bytes32 bzzHash, string calldata metadataUri) external {
+        require(bzzHash != bytes32(0), "Invalide hash");
 
+        publisherOf[bzzHash] = msg.sender;
+        metadataOf[bzzHash] = metadataUri;
+
+        emit ManifestPublished(msg.sender, bzzHash, metadataUri, block.timestamp);
+    }
 }
--- apps/swarm-registry/contracts/interface/IRegistry.sol
+++ apps/swarm-registry/contracts/interface/IRegistry.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 interface IRegistry {
-    function publishMenifest(bytes32 bzzHash, string calldata metadataUri) external;
+    function publishManifest(bytes32 bzzHash, string calldata metadataUri) external;
     function getMetadata(bytes32 bzzHash) external view returns (string memory);
     function getPublisher(bytes32 bzzHash) external view returns (address);
 }
