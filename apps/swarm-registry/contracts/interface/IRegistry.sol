ec197b985118d5794e57f410cdc2b33e032dd429
amochuko <7204868+amochuko@users.noreply.github.com>
2025-12-19 05:26:18 +0100

    add registry interface

---
+++ apps/swarm-registry/contracts/interface/IRegistry.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+interface IRegistry {
+    function publishMenifest(bytes32 bzzHash, string calldata metadataUri) external;
+    function getMetadata(bytes32 bzzHash) external view returns (string memory);
+    function getPublisher(bytes32 bzzHash) external view returns (address);
+}
