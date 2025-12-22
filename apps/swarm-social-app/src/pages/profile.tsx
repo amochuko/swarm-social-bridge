6832209d1e77e3954369e7623bef1eeec7d142d5
amochuko <7204868+amochuko@users.noreply.github.com>
2025-12-22 14:35:14 +0100

    add pages/profile

---
+++ apps/swarm-social-app/src/pages/profile.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { getRegistryClient } from "../sdk";
+import { useProviderStore } from "../store/useProviderStore";
+import { swarm } from "../swarm";
+
+type ProfilePageProps = {
+  postageId: string;
+};
+export function ProfilePage(props: ProfilePageProps) {
+  const provider = useProviderStore((s) => s.provider);
+
+  const [handle, setHandle] = useState("");
+  const [bio, setBio] = useState("");
+
+  async function saveProfile() {
+    const profile = {
+      type: "profile",
+      handle,
+      bio,
+      createdAt: Date.now(),
+    };
+
+    const swarmRegistry = await getRegistryClient();
+    const { bzzHash } = await swarm.uploadJSON(profile, props.postageId);
+    const signer = await provider?.getSigner();
+
+    await swarmRegistry.publishBatch({
+      items: [{ bzzHash: bzzHash.toString(), metadataUri: "swarm://profile" }],
+      nonce: BigInt(await signer!.getNonce()),
+    });
+
+    alert("Profile published!!");
+  }
+
+  return (
+    <div>
+      <h3>Create Profile</h3>
+      <input
+        type="text"
+        placeholder="Handle"
+        value={handle}
+        onChange={(e) => setHandle(e.target.value)}
+      />
+      <br />
+      <textarea
+        placeholder="Bio"
+        value={bio}
+        onChange={(e) => setBio(e.target.value)}
+      />
+      <br />
+      <button onClick={saveProfile}>Save Profile</button>
+    </div>
+  );
+}
