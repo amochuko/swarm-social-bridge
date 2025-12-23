4129c246366fc8485f3ca144ffe3cdd314f790db
amochuko <7204868+amochuko@users.noreply.github.com>
2025-12-23 17:21:48 +0100

    next routes into Layout

--- apps/swarm-social-app/src/main.tsx
+++ apps/swarm-social-app/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
+import Layout from "./components/Layout.tsx";
 import "./index.css";
 import { MessagePage } from "./pages/message.tsx";
 import { ProfilePage } from "./pages/profile.tsx";
@@ -9,11 +10,12 @@ import { ProfilePage } from "./pages/profile.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      {/* <App /> */}
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/message" element={<MessagePage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/message" element={<MessagePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
