61cead0850939dee454989455075cedd15682d9d
amochuko <7204868+amochuko@users.noreply.github.com>
2025-12-23 10:16:41 +0100

    add check for BEE_NODE_URL

--- apps/swarm-social-app/src/swarm.ts
+++ apps/swarm-social-app/src/swarm.ts
@@ -1,6 +1,12 @@
 import { createSwarmClient } from "@swarm-registry/registry-sdk";
+import { config } from "./utils/config";
 
+const { BEE_NODE_URL } = config.env;
+
+if (!BEE_NODE_URL) {
+  alert("BEE_NODE_URL is not defined in environment variables");
+}
 
 export const swarm = createSwarmClient({
-    beeNodeUrl: import.meta.env.VITE_BEE_NODE_URL,
+  beeNodeUrl: import.meta.env.VITE_BEE_NODE_URL,
 });
