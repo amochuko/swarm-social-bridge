b8913ae6375f6a2a668be686f33a1d54702755d8
amochuko <7204868+amochuko@users.noreply.github.com>
2025-12-23 16:33:04 +0100

    set up router

--- apps/swarm-social-app/src/main.tsx
+++ apps/swarm-social-app/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App.tsx";
+import "./index.css";
+import { MessagePage } from "./pages/message.tsx";
+import { ProfilePage } from "./pages/profile.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      {/* <App /> */}
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/message" element={<MessagePage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
